a0606d627a35123bf94a7c967fb7166a430a38c7
Didnelpsun <2675350965@qq.com>
2020-07-25 23:35:22 +0800

    初始化

---
+++ data_loader.py
@@ -0,0 +1,190 @@
+# 此文件用于数据载入
+import glob  # glob是python自己带的一个文件操作相关模块，用它可以查找符合自己目的的文件，类似于Windows下的文件搜索，支持通配符操作
+import os  # #os库是Python标准库，包含几百个函数,常用路径操作、进程管理、环境参数等几类。os.path子库以path为入口，用于操作和处理文件路径。
+
+# Python音频信号处理库函数librosa，提供了创建音乐信息检索系统所需的构建块
+import librosa
+# 数据分析模块numpy
+import numpy as np
+# Pytorch也被称为torch，是深度学习框架库，Tensorflow类似，FloatTensor是其基本数据单位
+import torch
+# sklearn是基础机器学习库，preprocessing为预处理库，LabelBinarizer用以one-hot编码转变，用以标签二值化，即将一般数据如真假值
+# 13534等数据变为01组合
+from sklearn.preprocessing import LabelBinarizer
+# DataLoader为数据加载器，本身是一个可迭代对象，使用iter()访问，不能使用next()访问，
+# 也可以使用iter(dataloader)返回的是一个迭代器，然后可以使用next访问
+# 并利用多进程来加速batch data的处理，使用yield来使用有限的内存
+
+# pytorch的数据加载到模型的操作顺序是这样的：
+# ① 创建一个 Dataset 对象
+# ② 创建一个 DataLoader 对象
+# ③ 循环这个 DataLoader 对象，将img, label加载到模型中进行训练
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data.dataset import Dataset
+
+# 从预处理库中进入对应的音频预处理对象，FEATURE_DIM为特征维，FFTSIZE为傅里叶变换大小，用于将数据转换为音频数据
+# FRAMES采样点数，SAMPLE_RATE为采样率
+from preprocess import (FEATURE_DIM, FFTSIZE, FRAMES, SAMPLE_RATE, world_features)
+# utility为个人定义工具集库，Normalizer为归一化对象，speakers为发音者对象，处理不同发音人
+from utility import Normalizer, speakers
+# random为随机数库，用来生成随机数
+import random
+
+
+# 构建音频数据集，将数据集输入转换为音频数据集
+class AudioDataset(Dataset):
+    """对音频数据集的文档字符串化"""
+
+    # 初始化音频数据，datadir为数据集文件路由
+    # 并定义音频数据集对象所拥有的方法与属性
+    def __init__(self, datadir: str):
+        # 对对象进行基本初始化
+        super(AudioDataset, self).__init__()
+        # datadir为对象数据集地址属性
+        self.datadir = datadir
+        # librosa.util.find_files获取目录或目录子树中已排序的(音频)文件列表
+        # 找到对应音频模型数据文件，ext表示后缀，npy就是其后缀，npy文件由python程序将数据处理而成，只能由python来解析
+        # files为对象的根据路由获取npy结尾的数据集的方法
+        self.files = librosa.util.find_files(datadir, ext='npy')
+        # 将spearkers对象使用LabelBinarizer的fit方法来变为one-hot数据作为编码器，即域标签。
+        # encoder为对象将发音者特征转化为one-hot特征值方法
+        self.encoder = LabelBinarizer().fit(speakers)
+
+    def __getitem__(self, idx):
+        # p为根据idx路由获取到对应的npy结尾数据集
+        p = self.files[idx]
+        # 返回path路由最后的文件名，如路由为D:\SVN\Git.png，那么文件名就是Git.png
+        filename = os.path.basename(p)
+        # 获取对应的发音者名，将文件名以_来分割，最多只分割一次，取第一个被分割出来的字符串，就是对应的发音者名字
+        # 这里是因为它的数据集的命名是这样命名所以这样处理
+        speaker = filename.split(sep='_', maxsplit=1)[0]
+        # tranform()的作用是通过找中心和缩放等实现标准化
+        # 获取标签，即将对应的发音者名通过transform方法标准化变为one-hot格式编码，取第一个数据。
+        label = self.encoder.transform([speaker])[0]
+        # 使用numpy.load方法来加载对应npy数据集文件
+        mcep = np.load(p)
+        # 使用torch.FloatTensor将npy数据集文件转为pytorch框架中的基本变量类型FloatTensor格式
+        mcep = torch.FloatTensor(mcep)
+        # 使用torch.unsqueeze解压数据，因为不用加入多余维数，所以第二个参数为0
+        mcep = torch.unsqueeze(mcep, 0)
+        # torch.tensor用于生成新的张量，即数据转换为对应的tensor数据，dtype属性为转换目标数据的类型，类型为LongTensor
+        # 对应的FloatTensor方法将数据变为FloatTensor
+        # 所以最后返回的数据为解压的音频数据集mecp，转换为LongTensor类型的名为speaker变量的speakers值（按键值对的键值取出）
+        # 以及转换为FloatTensor类型的标签
+        return mcep, torch.tensor(speakers.index(speaker), dtype=torch.long), torch.FloatTensor(label)
+
+    # speaker_encoder返回数据集的解码器，即域标签
+    def speaker_encoder(self):
+        return self.encoder
+
+    # __len__方法定义数据集对象的大小
+    def __len__(self):
+        return len(self.files)
+
+
+# 调用数据集的文件数据载入方法，datadir为数据集文件路由，批处理每次的并行数据为4条，进行shuffle操作，模式为训练
+# shuffile操作是针对处理大量和多进程操作时，为了优化网络和IO操作的处理方式，即对数据进行分区与合并加快数据处理，代价很大
+# 使用两个子进程处理数据
+def data_loader(datadir: str, batch_size=4, shuffle=True, mode='train', num_workers=2):
+    """如果模式是训练数据，它应该包含所有npy文件的训练集
+    或者，模式是测试，数据集路由应该只包含wav格式文件，即音频源文件
+    """
+
+    # 调用之前定义的转换音频数据集的方法AudioDataset将对应文件路由转换为可处理的数据集
+    dataset = AudioDataset(datadir)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    # 返回文件加载器
+    return loader
+
+
+class TestSet(object):
+    """对于测试数据的文档字符串化"""
+
+    def __init__(self, datadir: str):
+        super(TestSet, self).__init__()
+        self.datadir = datadir
+        # 定义对象norm方法为数据正则化方法
+        self.norm = Normalizer()
+
+    def choose(self):
+        '''为测试数据选择一个发音者'''
+
+        # 根据speakers这个序列中随机取出一个speaker作为目标发音者
+        r = random.choice(speakers)
+        return r
+
+    # 默认目标发音者为空值
+    def test_data(self, src_speaker=None):
+        '''为转换数据选择一个发音者'''
+        # 如果传入了目标发音者，即将这个参数赋值给r_s变量
+        if src_speaker:
+            r_s = src_speaker
+        # 如果没有参数传入
+        else:
+            # 就自动调用对象的choose方法随机选取一个发音者
+            r_s = self.choose()
+        # 将这个目标发音者的名称和原本的数据集地址拼接在一起作为新路径
+        # 因为这个数据集是以发音者的名字作为子集的，所以要找到对应发音者的子数据集必须这样处理
+        # 如果采用不同的数据集命名方式那么处理方式就会不同
+        p = os.path.join(self.datadir, r_s)
+        # 根据路由找到该路径下的所有wav格式文件
+        wavfiles = librosa.util.find_files(p, ext='wav')
+
+        res = {}
+        # 遍历这个数据集对象
+        for f in wavfiles:
+            # 获取对应的文件名
+            filename = os.path.basename(f)
+            # librosa.load方法f为文件地址，sr参数为采样率，如果保存原有采样率则赋值为None
+            # dtype为精度，将返回一个音频时间序列和一个音频采样率
+            # 因为音频采样率不被使用，所以以_作为变量名占位保存
+            # 该方法还有mono ：bool，是否将信号转换为单声道
+            # offset ：float，在此时间之后开始阅读（以秒为单位）
+            # duration：float，仅加载这么多的音频（以秒为单位）
+            wav, _ = librosa.load(f, sr=SAMPLE_RATE, dtype=np.float64)
+            f0, timeaxis, sp, ap, coded_sp = world_features(wav, SAMPLE_RATE, FFTSIZE, FEATURE_DIM)
+            coded_sp_norm = self.norm.forward_process(coded_sp.T, r_s)
+
+            if not res.__contains__(filename):
+                res[filename] = {}
+            res[filename]['coded_sp_norm'] = np.asarray(coded_sp_norm)
+            res[filename]['f0'] = np.asarray(f0)
+            res[filename]['ap'] = np.asarray(ap)
+        return res, r_s
+
+
+if __name__ == '__main__':
+    # t = TestSet('data/speakers_test')
+    # # mcep, f0, speaker = t[0]
+    # # print(speaker)
+    # # print(mcep)
+    # # print(f0)
+    # # print(np.ma.log(f0))
+    # d, speaker = t.test_data()
+
+    # for filename, content in d.items():
+    #     coded_sp_norm = content['coded_sp_norm']
+    #     print(content['coded_sp_norm'].shape)
+    #     f_len = coded_sp_norm.shape[1]
+    #     if  f_len >= FRAMES: 
+    #         pad_length = FRAMES-(f_len - (f_len//FRAMES) * FRAMES)
+    #     elif f_len < FRAMES:
+    #         pad_length = FRAMES - f_len
+
+    #     coded_sp_norm = np.hstack((coded_sp_norm, np.zeros((coded_sp_norm.shape[0], pad_length))))
+    #     print('after:' , coded_sp_norm.shape)
+    # print(t[1])
+    ad = AudioDataset('./data/processed')
+    print(len(ad))
+
+    data, s, label = ad[500]
+    print(data, label)
+    # loader = data_loader('./data/processed', batch_size=4)   
+
+    # for i_batch, batch_data in enumerate(loader):
+    #     # print(batch_data)
+    #     # print(batch_data[0])
+    #     print(batch_data[1])
+    #     print(batch_data[2])
+    #     if i_batch == 2:
+    #         break
---
+++ utility.py
@@ -0,0 +1,246 @@
+# 该文件为项目专门的工具类
+import numpy as np
+import pyworld as pw
+import os, shutil
+# glob为python标准库之一，可根据 Unix 终端所用规则找出所有匹配特定模式的路径名，但会按不确定的顺序返回结果
+import glob
+import librosa
+
+
+class Singleton(type):
+    # args参数为一个任意元素个数的元组，kwargs为一个任意元素个数的字典
+    def __init__(self, *args, **kwargs):
+        # 设置对象的__instance属性为空，这个属性代表对象的实例
+        self.__instance = None
+        super().__init__(*args, **kwargs)
+
+    # 赋值方法
+    def __call__(self, *args, **kwargs):
+        # 如果对象的实例属性为空，则调用方法构造实例属性
+        if self.__instance is None:
+            self.__instance = super().__call__(*args, **kwargs)
+            return self.__instance
+        # 否就直接返回
+        else:
+            return self.__instance
+
+
+class CommonInfo(metaclass=Singleton):
+    """一般信息的文档字符串化"""
+
+    # 类初始化方法，参数为数据文件路径
+    def __init__(self, datadir: str):
+        super(CommonInfo, self).__init__()
+        self.datadir = datadir
+
+    # @property装饰器就是负责把一个方法变成属性调用
+    @property
+    # 获取发音者标签组方法
+    def speakers(self):
+        """ 返回发音者组作为训练源
+        如 ['SF2', 'TM1', 'SF1', 'TM2']
+        """
+        # 将数据路径后加上*
+        p = os.path.join(self.datadir, "*")
+        # 调用glob.glob方法查询该目录下的所有文件，*代表匹配全部
+        all_sub_folder = glob.glob(p)
+        # 使用rsplit方法将获取的文件集通过指定分隔符从最后开始对字符串进行分割并返回一个列表，maxsplit为仅切分一次，并取第二个部分
+        # 如../marry/v1文件，就被分为../marry/和v1两个部分，[1]就是取v1这个部分
+        all_speaker = [s.rsplit('/', maxsplit=1)[1] for s in all_sub_folder]
+        # 将标签组重新排序
+        all_speaker.sort()
+        return all_speaker
+
+
+# 调用CommonInfo方法将数据地址传入，调用speakers属性获取对应标签组方法
+speakers = CommonInfo('data/speakers').speakers
+
+
+# 个性标准化方法
+class Normalizer(object):
+    """获取标准化的方法"""
+    def __init__(self, statfolderpath: str = './etc'):
+        # 定义文件集初始路由
+        self.folderpath = statfolderpath
+        # 将normalizer_dict方法作为属性赋值给类
+        self.norm_dict = self.normalizer_dict()
+
+    # 向前操作，下面的两个函数都是为了处理数据的相关标准
+    def forward_process(self, x, speakername):
+        # coded_sps_mean为编码平均值，coded_sps_std为编码标准差
+        mean = self.norm_dict[speakername]['coded_sps_mean']
+        std = self.norm_dict[speakername]['coded_sps_std']
+        # np.reshape方法将数组根据[行数,列数]的参数变为不同形状矩阵，而这里的-1表示是不知道行数多少
+        # 所以这样就可以将mean和std数据同时变为列向量形式的特征向量
+        mean = np.reshape(mean, [-1, 1])
+        std = np.reshape(std, [-1, 1])
+        x = (x - mean) / std
+
+        return x
+
+    # 向后操作
+    def backward_process(self, x, speakername):
+        # 取出对应名字的两个数据字典
+        mean = self.norm_dict[speakername]['coded_sps_mean']
+        std = self.norm_dict[speakername]['coded_sps_std']
+        mean = np.reshape(mean, [-1, 1])
+        std = np.reshape(std, [-1, 1])
+        x = x * std + mean
+
+        return x
+
+    # 返回一个发音者名对应发音者音频数据的字典
+    def normalizer_dict(self):
+        """返回所有发音者的标准化后参数"""
+
+        d = {}
+        # 迭代处理每个发声者数据，遍历speakers类中的所有发音者名
+        for one_speaker in speakers:
+            # 将数据路由后加上*.npz表示取出该目录下所有的npz类型文件
+            p = os.path.join(self.folderpath, '*.npz')
+            try:
+                # 根据路径取出对应的文件，如果遍历的子对象在对应的文件集中，就再取出作为文件路由
+                stat_filepath = [fn for fn in glob.glob(p) if one_speaker in fn][0]
+            except:
+                # 如果没有找到对应的文件就报错
+                raise Exception('====no match files!====')
+            # print(f'[load]: {stat_filepath}')
+            # 找到路由对应文件就加载该文件
+            t = np.load(stat_filepath)
+            # 并以对象的发音者名:音频文件作为存储方式来存储到字典中
+            d[one_speaker] = t
+
+        return d
+
+    # 音频转换函数，采用对数方式
+    def pitch_conversion(self, f0, source_speaker, target_speaker):
+        """基音转换的对数高斯归一化"""
+
+        # 分别取出源发音者与目标发音者的数据集平均值和标准差
+        mean_log_src = self.norm_dict[source_speaker]['log_f0s_mean']
+        std_log_src = self.norm_dict[source_speaker]['log_f0s_std']
+
+        mean_log_target = self.norm_dict[target_speaker]['log_f0s_mean']
+        std_log_target = self.norm_dict[target_speaker]['log_f0s_std']
+        # np.exp为e的幂次方，numpy.ma支持数值数组中包含掩码元素，用于处理错误和异常数据
+
+        # 掩码数组是将标准的多维数组numpy.ndarray和掩码相结合。掩码要么是nomask，表示与该掩码有关数组的所有值都是有效的。
+        # 要么是一个布尔值数组，用于确定关联数组的每个元素值是否有效。
+        # 当掩码中某个元素值为False，那么关联数组的对应元素是有效的，即被认为是未掩码的。
+        # 当掩码中某个元素值为True，那么关联数组的对应元素是无效的，即被认为是掩码的
+
+        # log就是取对数方法
+        f0_converted = np.exp((np.ma.log(f0) - mean_log_src) / std_log_src * std_log_target + mean_log_target)
+        # 返回转换结果
+        return f0_converted
+
+
+# 生成发音者npz格式数据字典
+class GenerateStatistics(object):
+    # 初始化对象，一个参数为folder，表示处理目录
+    def __init__(self, folder: str = './data/processed'):
+        self.folder = folder
+        # 定义保存npz数据文件的字典
+        self.include_dict_npz = {}
+        # 遍历speakers类，遍历值为所有发音者名字
+        for s in speakers:
+            # __contains__用于判断某键是否存在该字典中
+            # 如果s这个发音者名字不在定义的字典中，就将键加入字典，并赋值为空
+            if not self.include_dict_npz.__contains__(s):
+                self.include_dict_npz[s] = []
+            # 循环遍历folder路由下所有文件
+            for one_file in os.listdir(folder):
+                # 如果这个文件是以发音者名开始，并以npz结尾（即该文件是npz格式文件）
+                if one_file.startswith(s) and one_file.endswith('npz'):
+                    # 就将这个npz格式文件放入对应发音者键值的字典中
+                    self.include_dict_npz[s].append(one_file)
+
+    # @staticmethod注释表示该方法为静态方法，虽然名义由该模块管理，但是可以无视self，即对象，自由调用该方法
+    # 与其他编程语言的静态方法一致，所以下面的参数中没有带self参数
+    @staticmethod
+    def coded_sp_statistics(coded_sps):
+        # sp shape (D, T)
+        # np.concatenate表示按参数axis为轴合并为一个新数组，这里表示在第二维，即行方向合并coded_sps数组
+        coded_sps_concatenated = np.concatenate(coded_sps, axis=1)
+        # np.mean按计算axis参数维上的平均值，keepdims=False即不保持维数，可能会降维
+        # 这里计算数组行方向上的每一个平均值，并返回一个一维数组
+        coded_sps_mean = np.mean(coded_sps_concatenated, axis=1, keepdims=False)
+        # 与上面方法类似，np.std计算axis参数维上的标准差，在这里计算行方向的标准差，并不保持维数
+        coded_sps_std = np.std(coded_sps_concatenated, axis=1, keepdims=False)
+        return coded_sps_mean, coded_sps_std
+
+    @staticmethod
+    # 这里处理的是转换后数据
+    def logf0_statistics(f0s):
+        # 按照默认参数0，对f0s数组按列方向合并，在对结果进行掩码化与对数取值
+        log_f0s_concatenated = np.ma.log(np.concatenate(f0s))
+        # 分别取其平均值与标准差
+        log_f0s_mean = log_f0s_concatenated.mean()
+        log_f0s_std = log_f0s_concatenated.std()
+
+        return log_f0s_mean, log_f0s_std
+
+    def generate_stats(self, statfolder: str = 'etc'):
+        """
+        生成标准化的对于所有用户使用过的统计数据
+        输入sp与f0等类似数据
+        第一步，生成编码的平均标准差
+        第二步，生成f0即生成数据的平均标准差
+         """
+        # os.path.realpath方法用于返回该脚本的绝对路径
+        # 讲绝对路径与一个文件夹名路径连接，这个文件夹用来存放一些其他数据
+        etc_path = os.path.join(os.path.realpath('.'), statfolder)
+        # os.makedirs用于创建目录文件夹，这个方法如果父文件集都不存在则会一同创建
+        # exist_ok=True表示当要创建的文件夹已经存在时不会抛出OSError错误
+        os.makedirs(etc_path, exist_ok=True)
+        # .keys()以列表的形式返回一个字典的所有键名
+        # 遍历所有的保存npz格式文件的字典include_dict_npz键名
+        for one_speaker in self.include_dict_npz.keys():
+            f0s = []
+            coded_sps = []
+            # 将键名为onespeaker的npz数据保存到arr01中
+            arr01 = self.include_dict_npz[one_speaker]
+            # 如果这个数据为空数据，就跳出这个循环进行下一次遍历
+            if len(arr01) == 0:
+                continue
+            # 遍历这个npz数据中的所有数据项
+            for one_file in arr01:
+                # 将文件夹的路由与这个文件的路由名合并并加载文件
+                t = np.load(os.path.join(self.folder, one_file))
+                # 将这个文件的f0项，即生成样本项转换为特征列向量
+                f0_ = np.reshape(t['f0'], [-1, 1])
+                # 把这个生成样本的特征列向量放到列向量组中
+                f0s.append(f0_)
+                # 并把npz数据文件的coded_sp编码项也放入对应数组中
+                coded_sps.append(t['coded_sp'])
+            # 调用原有方法得到对应的特征向量组与编码组的平均值与标准差
+            log_f0s_mean, log_f0s_std = self.logf0_statistics(f0s)
+            coded_sps_mean, coded_sps_std = self.coded_sp_statistics(coded_sps)
+            # 打印对应数据
+            print(f'log_f0s_mean:{log_f0s_mean} log_f0s_std:{log_f0s_std}')
+            print(f'coded_sps_mean:{coded_sps_mean.shape}  coded_sps_std:{coded_sps_std.shape}')
+            # 将文件名命名为etc文件夹名/one_speaker变量值-stats.npz
+            filename = os.path.join(etc_path, f'{one_speaker}-stats.npz')
+            np.savez(filename,
+                     log_f0s_mean=log_f0s_mean, log_f0s_std=log_f0s_std,
+                     coded_sps_mean=coded_sps_mean, coded_sps_std=coded_sps_std)
+
+            print(f'[save]: {filename}')
+
+    def normalize_dataset(self):
+        '''normalize dataset run once!'''
+        norm = Normalizer()
+        files = librosa.util.find_files(self.folder, ext='npy')
+
+        for p in files:
+            filename = os.path.basename(p)
+            speaker = filename.split(sep='_', maxsplit=1)[0]
+            mcep = np.load(p)
+            mcep_normed = norm.forward_process(mcep, speaker)
+            os.remove(p)
+            np.save(p, mcep_normed)
+            print(f'[normalize]:{p}')
+
+
+if __name__ == "__main__":
+    pass
